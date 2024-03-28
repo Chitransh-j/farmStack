a12d70cab2f9fb25fef5a87ea0e3883839a7748a
Chitransh-j <chitranshjawere3@gmail.com>
2024-03-29 03:40:08 +0530

    amended deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverComponentsExternalPackages:["mongoose"]
+    experimental: {
+        serverComponentsExternalPackages: ["mongoose"]
     },
     images: {
         domains: ["lh3.googleusercontent.com"],
     },
     webpack(config) {
+        config.resolve.fallback = {
+            "mongodb-client-encryption": false ,
+            "aws4": false
+        },
         config.experiments = {
             ...config.experiments,
             topLevelAwait: true,
+
         };
         return config;
     },
-    
+
 };
 
 export default nextConfig;
